659afbab003a85f063fc70bcf18f5e745b921a09
Fhommand <85489785+Fhommand@users.noreply.github.com>
2022-01-28 14:47:29 +0100

    style updates

--- new_random_script.js
+++ new_random_script.js
@@ -5,7 +5,7 @@ const dataNotes = [
     id: 1,
     name: 'do',
     types: [
-      { type: "", color: '#000000' },
+      { type: '', color: '#000000' },
       { type: '#', color: '#ADACAC' }
     ]
   },
@@ -14,7 +14,7 @@ const dataNotes = [
     id: 2,
     name: 'ré',
     types: [
-      { type: "", color: '#FF0000' },
+      { type: '', color: '#FF0000' },
       { type: '#', color: '#FF8686' },
       { type: 'b', color: '#8D4646' }
     ]
@@ -24,7 +24,7 @@ const dataNotes = [
     id: 3,
     name: 'mi',
     types: [
-      { type: "", color: '#FB00FF' },
+      { type: '', color: '#FB00FF' },
       { type: 'b', color: '#854486' }
     ]
   },
@@ -33,7 +33,7 @@ const dataNotes = [
     id: 4,
     name: 'fa',
     types: [
-      { type: "", color: '#0004FF' },
+      { type: '', color: '#0004FF' },
       { type: '#', color: '#888AFE' }
     ]
   },
@@ -42,7 +42,7 @@ const dataNotes = [
     id: 4,
     name: 'sol',
     types: [
-      { type: "", color: '#00FFF3' },
+      { type: '', color: '#00FFF3' },
       { type: '#', color: '#82FFF9' },
       { type: 'b', color: '#4A9693' }
     ]
@@ -52,7 +52,7 @@ const dataNotes = [
     id: 5,
     name: 'la',
     types: [
-      { type: "", color: '#00FF0C' },
+      { type: '', color: '#00FF0C' },
       { type: '#', color: '#83FF89' },
       { type: 'b', color: '#47894A' }
     ]
@@ -62,7 +62,7 @@ const dataNotes = [
     id: 6,
     name: 'si',
     types: [
-      { type: "", color: '#FFFB00' },
+      { type: '', color: '#FFFB00' },
       { type: 'b', color: '#A1A04F' }
     ]
   },
@@ -70,15 +70,13 @@ const dataNotes = [
   {
     id: 7,
     name: 'soupir',
-    types: [
-      { type: "", color: '#FFFFFF' },
-    ]
+    types: [{ type: '', color: '#FFFFFF' }]
   }
 ]
 
 const dataTempo = [
   {
-    id : 1,
+    id: 1,
     name: 'largo',
     timeDivider: 0.833
   },
@@ -86,7 +84,7 @@ const dataTempo = [
   {
     id: 2,
     name: 'lento',
-    timeDivider : 0.999
+    timeDivider: 0.999
   },
 
   {
@@ -98,7 +96,7 @@ const dataTempo = [
   {
     id: 4,
     name: 'andante',
-    timeDivider : 1.466
+    timeDivider: 1.466
   },
 
   {
@@ -156,45 +154,45 @@ class Note {
 
 class Tempo {
   constructor (name, timeDivider) {
-    this.name = name;
-    this.timeDivider = timeDivider;
+    this.name = name
+    this.timeDivider = timeDivider
   }
 
-    getTimeDivider(){
-      return this.timeDivider
-    };
-  
+  getTimeDivider () {
+    return this.timeDivider
+  }
 }
 
-// création array note
+// creation array note
 
 let notes = []
 
+function arrayOfNotes () {
+  for (let i = 0; i < dataNotes.length; i++) {
+    let item = dataNotes[i]
+    for (let j = 0; j < item.types.length; j++) {
+      let note = new Note(item.name, item.types[j].color, item.types[j].type)
 
-
-for (let i = 0; i < dataNotes.length; i++) {
-  let item = dataNotes[i]
-  for (let j = 0; j < item.types.length; j++) {
-    let note = new Note(item.name, item.types[j].color, item.types[j].type)
-
-    notes.push(note)
+      notes.push(note)
+    }
   }
 }
+
+arrayOfNotes()
 
 console.log(notes)
 
 /// Program for a random score
-
-function getNumberOfNotes () {
-  var numberOfNotes = document.getElementById('number_of_notes').value;
-  console.log(numberOfNotes)
-  count = numberOfNotes;
-  display();
-}
-
 let count = 0
 
-const display = async () => {
+function getNumberOfNotes () {
+  var numberOfNotes = document.getElementById('number_of_notes').value
+  console.log(numberOfNotes)
+  count = numberOfNotes
+  display()
+}
+
+async function display () {
   for (let i = 0; i < count; i++) {
     let tempo = getTempo().getTimeDivider()
     await new Promise(r =>
@@ -205,56 +203,56 @@ const display = async () => {
 }
 
 function writeNotes () {
-  let note = randomizeElements(notes);
+  let note = randomizeElements(notes)
   console.log('note random = ' + note)
-  if (note.name == "soupir") {
-    document.getElementById('note').innerText = note.name;
+  if (note.name == 'soupir') {
+    document.getElementById('note').innerText = note.name
   } else {
-    document.getElementById('note').innerText = note.name + " " + note.type + " " + randomizeOctave(getRangeOfOctave());
+    document.getElementById('note').innerText =
+      note.name + ' ' + note.type + ' ' + randomizeOctave()
   }
-  document.getElementById('note').style.color = note.color;
+  document.getElementById('note').style.color = note.color
 }
 
 function randomizeElements (elem) {
-  let randomNote = elem[Math.floor(Math.random() * elem.length)];
+  let randomNote = elem[Math.floor(Math.random() * elem.length)]
 
   return randomNote
 }
 
 function randomizeTime (min, max) {
   console.log('min= ' + min + 'max=' + max)
-  let seconds = Math.floor(Math.random() * (max - min) + min);
+  let seconds = Math.floor(Math.random() * (max - min) + min)
 
   console.log('seconds=' + seconds)
   return seconds
 }
 
-function getRangeOfOctave () {
-  document.getElementById('errorOctaveMessage').innerText = "";
-  var octaveMin = parseInt(document.getElementById('min_octave').value);
-  var octaveMax = parseInt(document.getElementById('max_octave').value);
-  if (octaveMin > octaveMax) {
-    document.getElementById('errorOctaveMessage').innerText =
-      "Please enter a correct value, minimum octave can not be superior to maximum octave."
-  } else {
-
-    var rangeOfOctaves = [];
-    for (var i = octaveMin; i < octaveMax + 1; i++) {
-      rangeOfOctaves.push(i)
-    }
-    console.log('range octaves =' + rangeOfOctaves)
-    return rangeOfOctaves
+function getOctaves () {
+  document.getElementById('errorOctaveMessage').innerText = ''
+  let octaves = {
+    octaveMin: parseInt(document.getElementById('min_octave').value),
+    octaveMax: parseInt(document.getElementById('max_octave').value)
   }
+  if (octaves.octaveMin > octaves.octaveMax) {
+    document.getElementById('errorOctaveMessage').innerText =
+      'Please enter a correct value, minimum octave can not be superior to maximum octave.'
+  }
+  return octaves
 }
 
-function randomizeOctave (octaves) {
-  let randomOctave = octaves[Math.floor(Math.random() * octaves.length)];
+function randomizeOctave () {
+  let octavesChoosen = getOctaves()
+  let randomOctave = Math.floor(
+    Math.random() * (octavesChoosen.octaveMax - octavesChoosen.octaveMin) +
+      octavesChoosen.octaveMin
+  )
   return randomOctave
 }
 
 function getTempo () {
-  let tempoNameChosen = document.getElementById('choice_tempo').value;
-  let tempoToPlay;
+  let tempoNameChosen = document.getElementById('choice_tempo').value
+  let tempoToPlay
   dataTempo.forEach(tempo => {
     if (tempoNameChosen == tempo.name) {
       tempoToPlay = new Tempo(tempo.name, tempo.timeDivider)
